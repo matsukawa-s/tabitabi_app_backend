d00dcc36e1c14f6675c800d45e225b9dac2fa97e
matsukawa-s <syoudai.hand@gmail.com>
2020-12-28 22:41:21 +0900

    人気のスポットをスポットから仮取得

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -10,7 +10,7 @@ use App\Spot;
 class TopController extends Controller
 {
     public function index(){
-        // 本日のプラン取得
+        // 今日のプラン取得
         // 人気のプラン取得
         $popular_plans = Plan::select(DB::raw("*, favorite_count + number_of_views + referenced_number as score"))
                             ->orderby(DB::raw("score"),'desc')
@@ -18,12 +18,14 @@ class TopController extends Controller
                             ->get();
 
         // 人気のスポット取得
+        $popular_spots = Spot::limit(10)->get();
+
         // 最近見たプラン取得
 
         return response()->json([
             "today_plans" => [],
             "popular_plans" => $popular_plans,
-            "popular_spots" => [],
+            "popular_spots" => $popular_spots,
             "plan_history" => [],
         ]);
     }
