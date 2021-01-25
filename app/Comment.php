0e6b5fedd92673a1e9dc10682b0063a037aa8a20
macoto5636 <maakitak@icloud.com>
2021-01-25 20:11:00 +0900

    色々追加

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+        "user_id",
+        "plan_id",
+        "c_contents",
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
